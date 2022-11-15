3d5f6bff21e6afd3b9d55ad96e2e19ec28076858
Enrique Marina <eherraiz@myuax.com>
2022-11-15 12:09:12 +0100

    d

--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = '@!ez&y_lc(&*kc%(^h4r_@a$=wy9x!nq$2w3hpmeaz+#r!cs22'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
